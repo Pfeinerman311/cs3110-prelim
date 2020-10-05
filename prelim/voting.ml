fc87563c430284a0f9b7212da8be9283613cf20b
Parker Feinerman <pjf73@cornell.edu>
2020-10-05 15:38:04 -0400

    Finished basic implementation of plurality

--- prelim/voting.ml
+++ prelim/voting.ml
@@ -1,8 +1,5 @@
-(* TODO: change the name and NetID below to your own, then delete this TODO
-   comment. *)
-
 (** CS 3110 Fall 2020 Prelim
-    @author Your Name Here (your netid here) *)
+    @author Parker Feinerman (pjf73) *)
 
 (* TODO: complete the academic integrity statement below, then delete this
    TODO comment. *)
@@ -26,3 +23,57 @@
 
 
 (* TODO: Write your solution below, then delete this comment. *)
+type name = string
+
+type pballot = name
+
+type bballot = name list
+
+type candidate = {
+  name : name;
+
+  votes : int;
+}
+
+type tally = candidate list
+
+let compare c1 c2 =
+  match c1.votes - c2.votes with
+  | 0 -> String.compare c1.name c2.name
+  | x -> x
+
+let rec result_upd res results =
+  match results with
+  | [] -> [{name = res; votes = 1}]
+  | h::t -> if h.name = res then
+      {name = res; votes = h.votes + 1}::t else
+      h::(result_upd res t)
+
+let rec plur_calc ballots results =
+  match ballots with
+  | [] -> results
+  | h::t -> plur_calc t (result_upd h results)
+
+let plur_tally ballots =
+  plur_calc ballots [] |> List.sort_uniq compare |> List.rev
+
+let win_check w results =
+  match results with
+  | [] -> w.name
+  | h::t -> if w.votes = h.votes then
+      "No Winner" else w.name
+
+let plur_winner ballots =
+  match plur_tally ballots with
+  | [] -> failwith "No Votes"
+  | h::t -> win_check h t
+
+let candidate_to_string candidate =
+  String.concat "" ["("; candidate.name; ", "; 
+                    string_of_int candidate.votes; " votes)"]
+
+let rec tally_to_string tally =
+  match tally with
+  | [] -> ""
+  | h::t -> String.concat ", " 
+              [candidate_to_string h; tally_to_string t]
--- prelim/voting_test.ml
+++ prelim/voting_test.ml
@@ -1,8 +1,35 @@
 open OUnit2
 open Voting
 
+let tally_compare t1 t2 =
+  let u1 = List.sort_uniq compare t1 in
+  let u2 = List.sort_uniq compare t2 in
+  u1 = u2
+
+let plur_tally_test
+    (name : string)
+    (ballots : pballot list)
+    (expected_output : tally) : test =
+  name >:: (fun _ ->
+      assert_equal ~cmp:tally_compare ~printer:(tally_to_string)
+        expected_output (plur_tally ballots)) 
+
+let plur_winner_test
+    (name : string)
+    (ballots : pballot list)
+    (expected_output : name) : test =
+  name >:: (fun _ ->
+      assert_equal expected_output (plur_winner ballots))
+
+
+let t1b = ["David"; "David"; "Anne"; "David"; "Michael"; "Anne"; "Michael";
+           "David"; "Anne"; "David"]
+let t1t = [{name = "Anne"; votes = 3}; {name = "David"; votes = 5};
+           {name = "Michael"; votes = 2}]
 let tests = [
   (* TODO: add at least the 6 required tests here, and delete this comment. *)
+  plur_tally_test "Test 1" t1b t1t;
+  plur_winner_test "Test 2" t1b "David"
 ]
 
 let suite = "suite" >::: tests
