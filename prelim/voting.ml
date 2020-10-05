a795fae4929d27fe72c69b27ed5754f092fa5da2
Parker Feinerman <pjf73@cornell.edu>
2020-10-05 18:32:21 -0400

    Finished.

--- prelim/voting.ml
+++ prelim/voting.ml
@@ -1,9 +1,6 @@
 (** CS 3110 Fall 2020 Prelim
     @author Parker Feinerman (pjf73) *)
 
-(* TODO: complete the academic integrity statement below, then delete this
-   TODO comment. *)
-
 (************************************************************ 
 
    Academic Integrity Statement
@@ -21,76 +18,111 @@
 
  ************************************************************)  
 
-
-(* TODO: Write your solution below, then delete this comment. *)
+(** [name] represents the name of a a candidate *)
 type name = string
 
+(** [pballot] represents ballot of a plurality election *)
 type pballot = name
 
+(** [bballot] represents ballot of a Borda election *)
 type bballot = name list
 
+(** [candidate] represents a candidate with both a name and number
+    of votes *)
 type candidate = {
   name : name;
 
   votes : int;
 }
 
+(** [tally] represents the list of candidates returned in a tally *)
 type tally = candidate list
 
+(** [compare c1 c2] is the compare function for candidate variables.
+    Sorts by the number of votes first,
+    then defaults to alphabetical names.*)
 let compare c1 c2 =
   match c1.votes - c2.votes with
   | 0 -> String.compare c1.name c2.name
   | x -> x
 
-let rec result_upd res v results =
+(** [result_upd c v results] searches through the results and adds or 
+    updates the candidate c to have v more votes *)
+let rec result_upd c v results =
   match results with
-  | [] -> [{name = res; votes = v}]
-  | h::t -> if h.name = res then
-      {name = res; votes = h.votes + v}::t else
-      h::(result_upd res v t)
+  | [] -> [{name = c; votes = v}]
+  | h::t -> if h.name = c then
+      {name = c; votes = h.votes + v}::t else
+      h::(result_upd c v t)
 
+(** [plur_calc ballots results] goes through each element in a plurality 
+    ballot and calls result_upd to give the candidate one more vote *)
 let rec plur_calc ballots results =
   match ballots with
   | [] -> results
   | h::t -> plur_calc t (result_upd h 1 results)
 
+(** [plur_tally ballots] takes in a list of plurality ballots and calls
+    plur_calc to get the updated tally. Then unique sorts the list using
+    the above compare funtion and reverse the list to have winner first *)
 let plur_tally ballots =
   plur_calc ballots [] |> List.sort_uniq compare |> List.rev
 
+(** [win_check w results] checks the given possible winner against
+    the next candidate in results to check if there is a tie/no winner.
+    Returns the string "No Winner" if there is a tie *)
 let win_check w results =
   match results with
   | [] -> w.name
   | h::t -> if w.votes = h.votes then
       "No Winner" else w.name
 
+(** [plur_winner ballots] takes in a list of plurality ballots and calls
+    plur_tally to get the tally results, then calls win_check on the first
+    element to check if there is a tie *)
 let plur_winner ballots =
   match plur_tally ballots with
   | [] -> failwith "No Votes"
   | h::t -> win_check h t
 
+(** [borda_helper ballot results] goes through each element in a Borda 
+    ballot and calls result_upd to give the candidate the respective
+    number of more vote *)
 let rec borda_helper ballot results =
   match ballot with
   | [] -> results
   | h::t -> borda_helper t (result_upd h (List.length ballot) results)
 
+(** [borda_calc ballots results] goes through each element in a Borda 
+    ballot list and calls borda_helper to compute and update the Borda
+    ballot votes *)
 let rec borda_calc ballots results =
   match ballots with
   | [] -> results
   | h::t -> borda_calc t (borda_helper h results)
 
+(** [borda_tally ballots] takes in a list of Borda ballots and calls
+    borda_calc to get the updated tally. Then unique sorts the list using
+    the above compare funtion and reverse the list to have winner first *)
 let borda_tally ballots = 
   borda_calc ballots [] |> List.sort_uniq compare |> List.rev
 
+(** [borda_winner ballots] takes in a list of Borda ballots and calls
+    borda_tally to get the tally results, then calls win_check on the first
+    element to check if there is a tie *)
 let borda_winner ballots =
   match borda_tally ballots with
   | [] -> failwith "No Votes"
   | h::t -> win_check h t
 
-
+(** [candidate_to_strring candidate] converts a candidate variable into
+    a string of format (NAME, # votes) *)
 let candidate_to_string candidate =
   String.concat "" ["("; candidate.name; ", "; 
                     string_of_int candidate.votes; " votes)"]
 
+(** [tally_to_strring candidate] converts a tally variable into
+    a string of format (NAME, # votes), ... , (NAME, # votes) *)
 let rec tally_to_string tally =
   match tally with
   | [] -> ""
--- prelim/voting_test.ml
+++ prelim/voting_test.ml
@@ -1,11 +1,16 @@
 open OUnit2
 open Voting
 
+(** [tally_compare t1 t2] unique sorts tally t1 and t2 using the candidate
+    compare function and checks for equality *)
 let tally_compare t1 t2 =
   let u1 = List.sort_uniq compare t1 in
   let u2 = List.sort_uniq compare t2 in
   u1 = u2
 
+(** [plur_tally_test name ballots expected_output] constructs an
+    OUni test named [name] that asserts the quality of [expected_output]
+    with [plur_tally ballots]. *)
 let plur_tally_test
     (name : string)
     (ballots : pballot list)
@@ -14,6 +19,9 @@ let plur_tally_test
       assert_equal ~cmp:tally_compare ~printer:(tally_to_string)
         expected_output (plur_tally ballots)) 
 
+(** [plur_winner_test name ballots expected_output] constructs an
+    OUni test named [name] that asserts the quality of [expected_output]
+    with [plur_winner ballots]. *)
 let plur_winner_test
     (name : string)
     (ballots : pballot list)
@@ -21,6 +29,9 @@ let plur_winner_test
   name >:: (fun _ ->
       assert_equal expected_output (plur_winner ballots))
 
+(** [borda_tally_test name ballots expected_output] constructs an
+    OUni test named [name] that asserts the quality of [expected_output]
+    with [borda_tally ballots]. *)
 let borda_tally_test
     (name : string)
     (ballots : bballot list)
@@ -29,6 +40,9 @@ let borda_tally_test
       assert_equal ~cmp:tally_compare ~printer:(tally_to_string)
         expected_output (borda_tally ballots)) 
 
+(** [borda_winner_test name ballots expected_output] constructs an
+    OUni test named [name] that asserts the quality of [expected_output]
+    with [borda_winner ballots]. *)
 let borda_winner_test
     (name : string)
     (ballots : bballot list)
@@ -36,34 +50,54 @@ let borda_winner_test
   name >:: (fun _ ->
       assert_equal expected_output (borda_winner ballots))
 
+(** [t1_ballot] is the ballot for the first given test case
+    [t1_tally] is the expected tally result for the first given test case *)
+let t1_ballot = ["David"; "David"; "Anne"; "David"; "Michael";
+                 "Anne"; "Michael"; "David"; "Anne"; "David"]
+let t1_tally = [{name = "Anne"; votes = 3}; {name = "David"; votes = 5};
+                {name = "Michael"; votes = 2}]
 
-let t1b = ["David"; "David"; "Anne"; "David"; "Michael"; "Anne"; "Michael";
-           "David"; "Anne"; "David"]
-let t1t = [{name = "Anne"; votes = 3}; {name = "David"; votes = 5};
-           {name = "Michael"; votes = 2}]
-let t3b = ["David"; "David"; "Anne"; "David"; "Michael"; "Anne"; "Michael";
-           "David"; "Anne"; "Anne"]
+(** [t2_ballot] is the ballot for the second given test case
+    [t2_winner] is the expected tally result for the second given test case *)
+let t2_ballot = ["David"; "David"; "Anne"; "David"; "Michael";
+                 "Anne"; "Michael";"David"; "Anne"; "David"]
+let t2_winner = "David"
 
-let t4b = [["David"; "Anne"; "Michael"]; ["Anne"; "David"; "Michael"];
-           ["David"; "Anne"; "Michael"]; ["Anne"; "Michael"; "David"]]
-let t4t = [{name = "Anne"; votes = 10}; {name = "David"; votes = 9};
-           {name = "Michael"; votes = 5}]
+(** [t3_ballot] is the ballot for the third given test case
+    [t3_winner] is the expected tally result for the third given test case *)
+let t3_ballot = ["David"; "David"; "Anne"; "David"; "Michael";
+                 "Anne"; "Michael"; "David"; "Anne"; "Anne"]
+let t3_winner = "No Winner"
 
-let t5b = [["David"; "Anne"; "Michael"]; ["Anne"; "David"; "Michael"];
-           ["David"; "Anne"; "Michael"]; ["Michael"; "David"; "Anne"]]
+(** [t4_ballot] is the ballot for the fourth given test case
+    [t4_tally] is the expected tally result for the fourth given test case *)
+let t4_ballot = [["David"; "Anne"; "Michael"]; ["Anne"; "David"; "Michael"];
+                 ["David"; "Anne"; "Michael"]; ["Anne"; "Michael"; "David"]]
+let t4_tally = [{name = "Anne"; votes = 10}; {name = "David"; votes = 9};
+                {name = "Michael"; votes = 5}]
 
-let t6b = [["David"; "Anne"; "Michael"]; ["Anne"; "David"; "Michael"];
-           ["Michael"; "Anne"; "David"]; ["Michael"; "David"; "Anne"]]
+(** [t5_ballot] is the ballot for the fifth given test case
+    [t5_winner] is the expected tally result for the fifth given test case *)
+let t5_ballot = [["David"; "Anne"; "Michael"]; ["Anne"; "David"; "Michael"];
+                 ["David"; "Anne"; "Michael"]; ["Michael"; "David"; "Anne"]]
+let t5_winner = "David"
+
+(** [t6_ballot] is the ballot for the sixth given test case
+    [t6_winner] is the expected tally result for the sixth given test case *)
+let t6_ballot = [["David"; "Anne"; "Michael"]; ["Anne"; "David"; "Michael"];
+                 ["Michael"; "Anne"; "David"]; ["Michael"; "David"; "Anne"]]
+let t6_winner = "No Winner"
 
 
 let tests = [
-  (* TODO: add at least the 6 required tests here, and delete this comment. *)
-  plur_tally_test "Test 1" t1b t1t;
-  plur_winner_test "Test 2" t1b "David";
-  plur_winner_test "Test 3" t3b "No Winner";
-  borda_tally_test "Test 4" t4b t4t;
-  borda_winner_test "Test 5" t5b "David";
-  borda_winner_test "Test 6" t6b "No Winner";
+  (** These tests use the above variables for the test cases.
+      The string "No Winner" is the output in the case of no winner *)
+  plur_tally_test "Test 1 from Pg. 4" t1_ballot t1_tally;
+  plur_winner_test "Test 2 from Pg. 4" t2_ballot t2_winner;
+  plur_winner_test "Test 3 from Pg. 4" t3_ballot t3_winner;
+  borda_tally_test "Test 4 from Pg. 4" t4_ballot t4_tally;
+  borda_winner_test "Test 5 from Pg. 4" t5_ballot t5_winner;
+  borda_winner_test "Test 6 from Pg. 4" t6_ballot t6_winner;
 ]
 
 let suite = "suite" >::: tests
