6be8443d7a5b5a5166645676755def0e514c198d
Parker Feinerman <pjf73@cornell.edu>
2020-10-05 13:41:31 -0400

    Added prelim files

---
+++ prelim/voting_test.ml
@@ -0,0 +1,10 @@
+open OUnit2
+open Voting
+
+let tests = [
+  (* TODO: add at least the 6 required tests here, and delete this comment. *)
+]
+
+let suite = "suite" >::: tests
+
+let _ = run_test_tt_main suite
